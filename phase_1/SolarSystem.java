29539daf7026dd7e0ab72c78a220563734acbe37
ritwik srivastava <ritwik2504@gmail.com>
2023-03-20 11:17:03 +0100

    gui
    added the stars, changed dimensions to fit screen and added all the pictures to the planets, moons and the sun.

--- phase_1/SolarSystem.java
+++ phase_1/SolarSystem.java
@@ -1,17 +1,28 @@
 package phase_1;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
 
-
-public class SolarSystem extends JPanel {
-    
-    public void paintComponent(Graphics g) {
-
+public class SolarSystem extends JPanel 
+{
+    public void paintComponent(Graphics g) 
+    {
         Graphics2D g2D = (Graphics2D) g;
+        g2D.setStroke(new BasicStroke(1));
+        g.setColor(Color.WHITE);
 
-        Line2D.Double diagonalLine = new Line2D.Double(0, 0, 800,800);
+        Line2D.Double diagonalLine = new Line2D.Double(1400, 0, 0,750);
+        Line2D.Double diagonalLine1 = new Line2D.Double(0, 0, 1400,750);
+
+        for(int i = 0; i < 300; i++)
+        {
+            double x = Math.random()*1400;
+            double y = Math.random()*750;
+            Line2D.Double star = new Line2D.Double(x, y, x+1,y);
+            g2D.draw(star);
+        }
+       
         g2D.draw(diagonalLine);
+        g2D.draw(diagonalLine1);
     }
 }
--- phase_1/SolarSystemViewer.java
+++ phase_1/SolarSystemViewer.java
@@ -1,22 +1,29 @@
 package phase_1;
-
 import java.awt.*;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
-public class SolarSystemViewer {
-
-    public static void main(String[] args) {
-        
+public class SolarSystemViewer 
+{
+    public static void main(String[] args) 
+    {
         JFrame solarSystemFrame = new JFrame();
         solarSystemFrame.setTitle("Solar System Visualization");
-
+        solarSystemFrame.setBackground(new Color(000000));
         SolarSystem solarSystem = new SolarSystem();
 
-        final int SOLAR_SYSTEM_WIDTH = 800;
-        final int SOLAR_SYSTEM_HEIGHT = 800;
+        // JLabel planet;
+        // ImageIcon sun;
+
+        // sun = new ImageIcon(SolarSystemViewer.getResource("planet boss - sun.png"));
+        // planet = new JLabel(sun);
+        // SolarSystemViewer.add(planet);
+
+        final int SOLAR_SYSTEM_WIDTH = 1400;
+        final int SOLAR_SYSTEM_HEIGHT = 750;
 
         solarSystemFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -24,6 +31,7 @@ public class SolarSystemViewer {
         solarSystem.setPreferredSize(new Dimension(SOLAR_SYSTEM_WIDTH, SOLAR_SYSTEM_HEIGHT));
         solarSystemFrame.pack();
         solarSystemFrame.setResizable(false);
+        solarSystemFrame.setLocationRelativeTo(null);
         solarSystemFrame.setVisible(true);
     }
 }
